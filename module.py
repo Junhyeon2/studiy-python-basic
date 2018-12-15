a3538d3acb367ae7c020168a3cffc45c28b5c7cd
Junhyeon Kim <junhyeon47@gmail.com>
2018-12-15 21:51:18 +0900

    module

---
+++ module.py
@@ -0,0 +1,19 @@
+import random
+
+# 예제 1
+print('###### Example_01 ######')
+animals = ['cat', 'duck', 'bird']
+print(random.choice(animals))
+print(random.sample(animals, 2))
+print()
+
+# 예제 2
+print('###### Example_02 ######')
+print(random.randint(5, 10))
+print()
+
+# 예제 3
+print('###### Example_03 ######')
+cards = ['hart', 'clover', 'spade']
+print(random.choice(cards), 'guilty')
+print()
